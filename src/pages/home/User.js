e305eb049cbad311d996833b20ad97099bc3b1df
bayuuto <bayuuto@XLP017009.xlplanet.co.id>
2019-04-24 09:21:37 +0700

    hapus contoh bawah tambah a

--- src/pages/home/User.js
+++ src/pages/home/User.js
@@ -4,7 +4,7 @@ import { Table, Row, Col, Form, Modal, Button, Card, Jumbotron } from 'react-boo
 import { api } from '../../config/Services';
 import { GetRequest } from '../../services/RequestService';
 import ErrorPage from '../error/Error404';
-
+/*a*/
 class Layout extends Component {
 	constructor(props) {
         super(props);
